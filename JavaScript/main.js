8bcbbc816f3b091b6caa0f51eab2ef8eab58f3bf
Faro-Enuff <f.roth2503@gmail.com>
2021-07-19 11:25:23 +0200

    Code Cleaning + CSS Adds

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,35 +3,50 @@
 ////////
 ////
 
-const createUrl = (artistName) => {
-  const person = (artistName) => {
+let artistNameText = '';
+function artistCatch() {
+  artistNameText = document.getElementById('artistCall').value;
+  alert(artistNameText);
+}
+
+document
+  .getElementById('searchButton')
+  .addEventListener('click', artistCatch());
+
+console.log(`artistNameSearch`, artistNameText);
+
+// artistNameSearch = 'Peter Fox';
+
+const createUrl = (code, artistName) => {
+  const person = artistName => {
     let artist = {
       q: artistName,
-      key: "ndqiGGFVIuiLYHmjQExU",
-      secret: "ADOGBtMyjsEJjQkyVcoNQTASKAuLfdCW",
+      key: 'ndqiGGFVIuiLYHmjQExU',
+      secret: 'ADOGBtMyjsEJjQkyVcoNQTASKAuLfdCW',
       per_page: 100,
     };
     return artist;
   };
-  console.log(person(artistName));
 
-  let queryParams = "";
+  let queryParams = '';
 
   queryParams =
-    "?" +
+    '?' +
     Object.keys(person(artistName))
-      .map((key) => `${key}=${person(artistName)[key]}`)
-      .join("&");
+      .map(key => `${key}=${person(artistName)[key]}`)
+      .join('&');
 
-  console.log(queryParams);
+  // console.log(queryParams);
 
-  const url = "https://api.discogs.com/database/search" + queryParams;
+  const url = code + queryParams;
 
   return url;
 };
+const testUrl = createUrl(artistNameText);
+console.log(`testUrl`, testUrl);
 
-const example1 = createUrl("Eminem");
-console.log(example1);
+// const example1 = createUrl('Eminem');
+// console.log(example1);
 
 ////
 ////////
@@ -43,20 +58,23 @@ console.log(example1);
 ////
 
 var requestOptions = {
-  method: "GET",
-  redirect: "follow",
+  method: 'GET',
+  redirect: 'follow',
 };
 
-fetch(createUrl("Rihanna"), requestOptions)
-  .then((response) => response.json())
-  .then((artists) => {
+fetch(
+  createUrl('https://api.discogs.com/database/search', artistNameText),
+  requestOptions
+)
+  .then(response => response.json())
+  .then(artists => {
     console.log(`artists`, artists);
     let cleanData = artists.results;
     console.log(`cleanData`, cleanData);
     createMainContentTable(cleanData);
     createAlbumContentTable(cleanData);
   })
-  .catch((error) => console.log("error", error));
+  .catch(error => console.log('error', error));
 
 ////
 ////////
@@ -68,124 +86,135 @@ fetch(createUrl("Rihanna"), requestOptions)
 ////
 
 let createMainContentTable = function (cleanData) {
-  ///LINK TO TBODY
-  var tbody = document.getElementById("mainContentTableBody");
-
-  var row = document.createElement("tr");
-
-  var releaseArray = [];
+  //Preparing Data for Artist, Genre, Releases
+  const releaseArray = [];
+  const genresArray = [];
+  const artistArray = [];
 
   for (let i = 0; i < cleanData.length; i++) {
-    ///ARTIST COLUMN
-    if (cleanData[i].type === "artist") {
-      var cell = document.createElement("td");
-      cell.innerHTML = cleanData[i].title;
-      ///GENRE COLUMN
-    } else if (cleanData[i].type === "release") {
-      releaseArray.push(cleanData[i].title);
-      if (releaseArray.length <= 1) {
-        var cell = document.createElement("td");
-        cell.innerHTML = cleanData[i].genre[0];
+    ///ARTIST Array
+    if (cleanData[i].type === 'artist') {
+      if (!artistArray.includes(cleanData[i].title)) {
+        artistArray.push(cleanData[i].title);
       }
-      //LABEL COLUMN
-    } else if (cleanData[i].type === "label") {
-      var cell = document.createElement("td");
+      var cell = document.createElement('td');
       cell.innerHTML = cleanData[i].title;
-    }
-    cell.setAttribute("class", "align-middle");
-    row.appendChild(cell);
-  }
-  // COUNT RELEASES
-
-  var releases = function () {
-    var releaseArrayClean = [];
-    releaseArray.forEach((c) => {
-      if (!releaseArrayClean.includes(c)) {
-        releaseArrayClean.push(c);
+      ///Release & Genre Array
+    } else if (cleanData[i].type === 'release') {
+      if (!releaseArray.includes(cleanData[i].master_id)) {
+        releaseArray.push(cleanData[i].master_id);
+        cleanData[i].genre.forEach(genres => {
+          if (!genresArray.includes(genres)) {
+            genresArray.push(genres);
+          }
+        });
       }
-    });
-    console.log(releaseArrayClean);
-
-    return [releaseArrayClean, releaseArrayClean.length];
-  };
-  /// RELEASES COLUMN
-  var cell2 = document.createElement("td");
-  cell2.innerHTML = releases()[1];
-  cell2.setAttribute("class", "align-middle");
-  row.appendChild(cell2);
-
-  /// SoundCloud
-  var cell = document.createElement("td");
-  var scIconImg = document.createElement("img");
-  // sIconImg.setAttribute("src", "../img/SoundCloud Logo Dark.png");
-  function setAttributes(el, attrs) {
-    for (var key in attrs) {
-      el.setAttribute(key, attrs[key]);
     }
   }
-  setAttributes(scIconImg, {
-    src: "../img/Soundcloud40.png",
-    scrolling: "no",
-    style: "cursor: pointer",
-    style: "width: 45px; height: 45px",
-    style: "table",
-    style: "border-radius: 30px",
+
+  let stringGenres = genresArray[0];
+  genresArray.forEach(genres => {
+    stringGenres = stringGenres + ', ' + genres;
   });
 
-  //FUNCTION FOR ARTIST - INSERT A PARAMETER LATER
-  scIconImg.onclick = function () {
-    window.open("https://soundcloud.com/eminemofficial", "_blank");
-  };
-  cell.appendChild(scIconImg);
-  row.appendChild(cell);
+  //Main Arrays for Table
+  let arrTH = ['th1', 'th2', 'th3', 'th4', 'th5', 'th6'];
+  let cleanDataArtistTable = [
+    artistArray[0],
+    stringGenres,
+    releaseArray.length,
+  ];
 
-  //Spotify IMG
-  var cell = document.createElement("td");
-  var spIconImg = document.createElement("img");
-  setAttributes(spIconImg, {
-    src: "../img/Spotify Icon Black.png",
-    scrolling: "no",
-    style: "cursor: pointer",
-    style: "width: 45px; height: 45px",
-  });
-  spIconImg.onclick = function () {
-    window.open("https://open.spotify.com/search/eminem", "_blank");
-  };
-  cell.appendChild(spIconImg);
-  row.appendChild(cell);
+  //Table Creation
 
-  //Beatport IMG
-  var cell = document.createElement("td");
-  var bpIconImg = document.createElement("img");
-  setAttributes(bpIconImg, {
-    src: "../img/Beatport.png",
-    scrolling: "no",
-    style: "cursor: pointer",
-    style: "width: 45px; height:45px",
-  });
-  bpIconImg.onclick = function () {
-    window.open("https://www.beatport.com/artist/eminem/48400", "_blank");
-  };
-  cell.appendChild(bpIconImg);
-  row.appendChild(cell);
+  //Build Body & Row
+  const tbody = document.getElementById('mainContentTableBody');
+  const row = document.createElement('tr');
 
-  tbody.appendChild(row);
+  //Build Cells + Insert Content
+  for (let i = 0; i < arrTH.length; i++) {
+    // First Three Headings
+    if (i <= 2) {
+      const cell = document.createElement('td');
+      cell.innerHTML = cleanDataArtistTable[i];
+      row.appendChild(cell);
+    } else if (i === 3) {
+      /// SoundCloud
+      const cell = document.createElement('td');
+      const scIconImg = document.createElement('img');
+      // sIconImg.setAttribute("src", "../img/SoundCloud Logo Dark.png");
+      function setAttributes(el, attrs) {
+        for (var key in attrs) {
+          el.setAttribute(key, attrs[key]);
+        }
+      }
+      setAttributes(scIconImg, {
+        src: 'img/Soundcloud40.png',
+        scrolling: 'no',
+        style:
+          'width: 45px; height: 45px; border-radius: 30px; table;cursor: pointer ',
+      });
+      //FUNCTION FOR ARTIST - INSERT A PARAMETER LATER
+      scIconImg.onclick = function () {
+        window.open(
+          createUrl('https://soundcloud.com/search', artistNameSearch),
+          '_blank'
+        );
+      };
+      cell.appendChild(scIconImg);
+      row.appendChild(cell);
+    } else if (i === 4) {
+      //Spotify IMG
+      const cell = document.createElement('td');
+      const spIconImg = document.createElement('img');
+      setAttributes(spIconImg, {
+        src: 'img/SpotifyIconBlack.png',
+        scrolling: 'no',
+        style: 'width: 45px; height: 45px; cursor: pointer',
+      });
+      spIconImg.onclick = function () {
+        window.open(
+          'https://open.spotify.com/search/' + artistNameSearch,
+          '_blank'
+        );
+      };
+      cell.appendChild(spIconImg);
+      row.appendChild(cell);
+    } else if (i === 5) {
+      //Beatport IMG
+      const cell = document.createElement('td');
+      const bpIconImg = document.createElement('img');
+      setAttributes(bpIconImg, {
+        src: 'img/Beatport.png',
+        scrolling: 'no',
+        style: 'width: 45px; height:45px; cursor: pointer; border-radius: 30px',
+      });
+      bpIconImg.onclick = function () {
+        window.open(
+          createUrl('https://www.beatport.com/search', artistNameSearch),
+          '_blank'
+        );
+      };
+      cell.appendChild(bpIconImg);
+      row.appendChild(cell);
+    }
+    tbody.appendChild(row);
+  }
 
   //Album-Cover_Column
   const checkArray = [];
   for (let i = 0; i < cleanData.length; i++) {
     var check = cleanData[i].master_id;
     if (!checkArray.includes(check) && checkArray.length <= 4) {
-      var imgCover = document.createElement("img");
+      var imgCover = document.createElement('img');
       imgCover.src = cleanData[i].cover_image;
-      imgCover.setAttribute("class", "coverImg");
-      var src = document.getElementById("leftCoverColumn");
+      imgCover.setAttribute('class', 'coverImg');
+      var src = document.getElementById('leftCoverColumn');
       src.appendChild(imgCover);
       checkArray.push(check);
     }
   }
   console.log(checkArray);
-  console.log(releases()[0][0]);
 };
 
 ////
@@ -205,7 +234,7 @@ let createAlbumContentTable = function (cleanData) {
   const checkArrayId = [];
   for (let i = 0; i < cleanData.length; i++) {
     var checkId = cleanData[i].master_id;
-    if (cleanData[i].type === "release" && !checkArrayId.includes(checkId)) {
+    if (cleanData[i].type === 'release' && !checkArrayId.includes(checkId)) {
       cleanDataAlbum.push(cleanData[i].title);
       cleanDataGenre.push(cleanData[i].genre[0]);
       cleanDataYear.push(cleanData[i].year);
@@ -216,18 +245,22 @@ let createAlbumContentTable = function (cleanData) {
 
   //Create a String out of the Format Arrays
   const cleanDataFormatString = [];
+  const helpArray = [];
   for (let i = 0; i < cleanDataFormat.length; i++) {
-    var stringFormat = "";
+    let stringFormat = '';
     for (let j = 0; j < cleanDataFormat[i].length; j++) {
-      if (j < cleanDataFormat[i].length - 1) {
-        stringFormat = stringFormat + cleanDataFormat[i][j] + ", ";
+      if (!helpArray.includes(cleanDataFormat[i][j])) {
+        helpArray.push(cleanDataFormat[i][j]);
       }
-      stringFormat = stringFormat + cleanDataFormat[i][j];
+    }
+    for (let k = 0; k < helpArray.length; k++) {
+      if (k === helpArray.length - 1) {
+        stringFormat = stringFormat + helpArray[k];
+      }
+      stringFormat = stringFormat + helpArray[k] + ', ';
     }
     cleanDataFormatString.push(stringFormat);
   }
-
-  console.log(`cleanDataFormatString`, cleanDataFormatString);
 
   // CLEAN DATA FOR TABLE
   const cleanDataAlbumTable = [
@@ -238,14 +271,14 @@ let createAlbumContentTable = function (cleanData) {
   ];
   console.log(`cleanDataAlbumTable`, cleanDataAlbumTable);
 
-  arrTH = ["th1", "th2", "th3", "th4"];
+  arrTH = ['th1', 'th2', 'th3', 'th4'];
 
   let i = 0;
   while (i < cleanDataAlbumTable[0].length) {
-    var tbody = document.getElementById("albumContentTableBody");
-    var row = document.createElement("tr");
+    var tbody = document.getElementById('albumContentTableBody');
+    var row = document.createElement('tr');
     for (let j = 0; j < arrTH.length; j++) {
-      var cell = document.createElement("td");
+      var cell = document.createElement('td');
       cell.innerHTML = cleanDataAlbumTable[j][i];
       row.appendChild(cell);
     }
@@ -257,10 +290,10 @@ let createAlbumContentTable = function (cleanData) {
 // let albumTable = createAlbumContentTable();
 
 let hideShow = function () {
-  var x = document.getElementById("albumContentTable");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var x = document.getElementById('albumContentTable');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   } else {
-    x.style.display = "block";
+    x.style.display = 'block';
   }
 };
